ae825b9502d209e814556c7b288ef930f334c687
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-07-26 00:04:08 +1000

    Update scopus

--- scripts/bibtex.js
+++ scripts/bibtex.js
@@ -42,7 +42,16 @@ async function gettiddlerEID(eid, item, host) {
             var span = document.createElement("span");
             span.classList.add("tw-icon");
             span.innerText = "📖";
-            item.querySelector("div.refAuthorTitle, td[data-type='docTitle']").prepend(span);
+            var label = item.querySelector("label");
+            if (label !== null) {
+                if (label.querySelector("span")) {
+                    label.appendChild(span);
+                } else {
+                    label.parentNode.insertBefore(span, label.nextSibling);
+                }
+            } else {
+                item.querySelector("div.refAuthorTitle, td[data-type='docTitle']").prepend(span);
+            }
             item.style.background = "#e6e6e666";
         }
     } catch (error) {
@@ -149,17 +158,38 @@ function scholar(host) {
 function scopus(host) {
     // Whole page
     var eid_el = document.querySelector("input#currentRecordPageEID, input#cite");
-    if (eid_el !== undefined) {
+    if (eid_el !== undefined && eid_el !== null) {
         var eid = eid_el.value;
         gettiddler(eid, "eid", host);
     }
     // for reference and citation
-    var items = document.querySelectorAll("tr.referencesUL, tr.searchArea");
+    var items = document.querySelectorAll("tr.referencesUL, tr.searchArea, tr[class*='TableItems-module']:has( > td > label)");
     for (let i = 0; i < items.length; i++) {
-        var eid = items[i].querySelector("input").value;
-        
+        var eid;
+        var label = items[i].querySelector("td > label[for]");
+        if (label !== null) {
+            eid = label.getAttribute("for").replace("document-",'');
+        } else {
+            eid = items[i].querySelector("input").value;
+        }
         gettiddlerEID(eid, items[i], host);
         //console.log(eid);
+    }
+    // for doi
+    let infos = document.querySelectorAll("div[class*='SourceInfo-module'] > div > dl > dt");
+    for (let i = 0; i < infos.length; i++) {
+        if (infos[i].innerText == "DOI") {
+            let doi_ele = infos[i].nextSibling;
+            let doi = doi_ele.innerText;
+            let doi_link = document.createElement("a");
+            doi_link.innerHTML  = doi;
+            doi_link.setAttribute("href", "https://doi.org/" + doi);
+            doi_link.setAttribute("target", "_blank");
+            var dt_ele = doi_ele.parentElement;
+            dt_ele.removeChild(dt_ele.lastElementChild);
+            dt_ele.appendChild(doi_link);
+            
+        }
     }
     //console.log(items.length);
 }
