6ed4c06962016b6053b41558ae30f4e068281e08
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-07-31 12:08:52 +1000

    Test again

--- scripts/background.js
+++ scripts/background.js
@@ -4,10 +4,16 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    // if (request.from == "webpage"){
-        // chrome.runtime.sendMessage({
-            // from: "worker", 
-            // message: request.title});
-    // }
+    if (request.from == "webpage"){
+      
+        (async () => {
+          const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+          const response = await chrome.tabs.sendMessage(tab.id, {
+            from: "worker", 
+            message: request.title});
+          // do something with response here, not outside the function
+          console.log(response);
+        })();
+    }
   }
 );
